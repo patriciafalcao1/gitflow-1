b8fc04fb6b3b6aa0f76bc240491ef35a4b14188a
Ricardo Batista <emacs.copo@gmail.com>
2016-08-28 22:52:06 +0100

    Add data

--- gitflowolx.js
+++ gitflowolx.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $.get("participants.json", function(data) {
         $.each(data, function(index, value) {
             var template = $('#template').clone();
+            template.css('visibility', 'visible');
+            template.find('name').html(value.name);
+            template.find('.depart').html(value.departament + ' ' + value.email);
             $("#container").append(template);
         });
     });

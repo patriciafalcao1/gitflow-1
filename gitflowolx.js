200795d98824e4e98e3da265beb9a4e826f07234
Ricardo Batista <emacs.copo@gmail.com>
2016-08-28 22:54:08 +0100

    Remove id from template

--- gitflowolx.js
+++ gitflowolx.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $.get("participants.json", function(data) {
         $.each(data, function(index, value) {
             var template = $('#template').clone();
+            template.id = '';
             template.css('visibility', 'visible');
             template.find('name').html(value.name);
             template.find('.depart').html(value.departament + ' ' + value.email);

14777324c6cd09854a482d3f27e2bfd89924a579
Ricardo Batista <emacs.copo@gmail.com>
2016-08-28 22:44:57 +0100

    Add loop

--- gitflowolx.js
+++ gitflowolx.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $.get("participants.json", function(data) {
-        console.log(data);
+        $.each(data, function(index, value) {
+            var template = $('#template').clone();
+            $("#container").append(template);
+        });
     });
 });
